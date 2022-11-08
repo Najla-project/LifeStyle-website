81def2108bf08bfcb7ba9aa7525c8a95216289b7
Najla-project <117730909+Najla-project@users.noreply.github.com>
2022-11-08 13:10:38 +0200

    Add files via upload

---
+++ JS/LifeStyle.js
@@ -0,0 +1,21 @@
+
+let name= document.getElementById("Diet");
+let rendervalue= document.getElementById("Diet");
+
+function ShowOnClick(){
+    rendervalue.innerHTML=`${name.value}`
+}
+//    switch (name){
+//     case 'Diet':
+//         name.Style.display= "block";
+//         break;
+//     case 'Gym':
+//         name.Style.display= "block";
+//         break;
+//     case 'start':
+//         name.Style.display= "block";
+//         break;
+//         default:
+//             name.Style.display="none";
+//             break;
+//    }
